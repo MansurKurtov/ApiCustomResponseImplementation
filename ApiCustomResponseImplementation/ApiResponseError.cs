15abe64b22c75e27bf52506f05ffa700d3174822
Mansur <kurtov.mansur@gmail.com>
2021-09-14 16:51:20 +0500

    Add project files.

---
+++ ApiCustomResponseImplementation/ApiResponseError.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net;
+
+namespace ApiCustomResponseImplementation
+{
+    /// <summary>
+    /// This source created by Bunyod
+    /// </summary>
+    public class ApiResponseError
+    {
+        /// <summary>
+        ///     Response error model
+        /// </summary>
+        public ApiResponseError()
+        {
+        }
+
+        /// <summary>
+        ///     Response error model
+        /// </summary>
+        /// <param name="errorEnum">Error enum</param>
+        public ApiResponseError(Enum errorEnum)
+        {
+            ErrorCode = (int)(object)errorEnum;
+            //ErrorMessage = EnumSynonymProvider.Get(errorEnum);
+            ErrorMessage = errorEnum.ToString();
+
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="errorEnum"></param>
+        /// <param name="errorMessage"></param>
+        public ApiResponseError(Enum errorEnum, string errorMessage)
+        {
+            ErrorCode = (int)(object)errorEnum;
+            //ErrorMessage = $"{EnumSynonymProvider.Get(errorEnum)}  {errorMessage}";
+            ErrorMessage = errorEnum.ToString();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="statusCode"></param>
+        /// <param name="errorMessage"></param>
+        public ApiResponseError(HttpStatusCode statusCode, string errorMessage)
+        {
+            ErrorCode = (int)statusCode;
+            ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        ///     Response error model
+        /// </summary>
+        /// <param name="errorCode">Response error code</param>
+        /// <param name="errorMessage">Response error message</param>
+        public ApiResponseError(int errorCode, string errorMessage)
+        {
+            ErrorCode = errorCode;
+            ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="httpStatusCode"></param>
+        /// <param name="errorEnum"></param>
+        public ApiResponseError(HttpStatusCode httpStatusCode, Enum errorEnum)
+        {
+            //if (HttpContextHelper.Current?.Response != null)
+              //  HttpContextHelper.Current.Response.StatusCode = (int)httpStatusCode;
+
+            ErrorCode = (int)httpStatusCode;
+            //ErrorMessage = EnumSynonymProvider.Get(errorEnum);
+            ErrorMessage = errorEnum.ToString();
+        }
+
+        /// <summary>
+        /// </summary>
+        public int ErrorCode { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ ApiCustomResponseImplementation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ApiCustomResponseImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test case#1:");
+            var testCase1 = GetStrings(-1);
+            if (testCase1.IsSuccess)
+                Console.WriteLine(testCase1.Result);
+            else Console.WriteLine($"Error:{testCase1.Error.ErrorMessage}");
+
+            Console.WriteLine("Test case#2:");
+            var testCase2 = GetStrings(24);
+            if (testCase2.IsSuccess)
+                Console.WriteLine(testCase2.Result);
+            else Console.WriteLine($"Error:{testCase2.Error.ErrorMessage}");
+
+            Console.ReadKey();
+        }
+
+        public static ApiResponse<string> GetStrings(int i)
+        {
+            if (i < 0)
+                return new ApiResponseError(-1, $"Integer value less then zero:{i})");
+
+            return $"This number bigger then zero:{i}";
+        }
+    }
+}
